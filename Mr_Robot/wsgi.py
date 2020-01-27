d68a0c0566025be9eab8ca2d4c60a626c4233509
Omar Al Shayji <omaralshayji@Omars-MacBook-Pro.local>
2020-01-27 16:11:27 +0300

    First commit

---
+++ Mr_Robot/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Mr_Robot project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mr_Robot.settings')
+
+application = get_wsgi_application()
